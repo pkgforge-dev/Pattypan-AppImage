29d46d018d4cff61dc40461091fb1223edaf8285
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-13 19:21:10 -0400

    set version

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,7 +18,12 @@ get-debloated-pkgs --add-common --prefer-nano
 # If the application needs to be manually built that has to be done down here
 mkdir -p ./AppDir/bin
 cd ./AppDir/bin
-wget https://github.com/yarl/pattypan/releases/download/v26.02/pattypan-java-26-02.jar
+latest_jar=$(wget --retry-connrefused --tries=30 \
+	https://api.github.com/repos/yarl/pattypan/releases -O - \
+	| sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*/pattypan-java-.*.jar"
+)
+wget  --retry-connrefused --tries=30 "$latest_jar"
 echo '#!/bin/sh
 exec java -jar "$APPDIR"/bin/pattypan-java-26-02.jar "$@"' > ./pattypan
 chmod +x ./*
+echo "$latest_jar" | awk -F'/' '{print $(NF-1)}' > ~/version 
