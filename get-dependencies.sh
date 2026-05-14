63fa8a53434c3b7cb8199b2856c321e08787b1fa
Link4Electronics <link4electronics@gmail.com>
2026-05-14 19:12:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,8 +17,7 @@ make-aur-package gradle8
 make-aur-package java-openjfx
 
 # If the application needs to be manually built that has to be done down here
-mkdir -p ./AppDir/bin
-cd ./AppDir/bin
+mkdir -p ./AppDir/bin && cd ./AppDir/bin
 latest_jar=$(wget --retry-connrefused --tries=30 \
 	https://api.github.com/repos/yarl/pattypan/releases -O - \
 	| sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*/pattypan-java-.*.jar"
