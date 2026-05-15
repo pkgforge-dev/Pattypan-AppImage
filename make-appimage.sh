e05ec0dd9391be94f47320d5a24a32147623e2e1
Link4Electronics <link4electronics@gmail.com>
2026-05-14 22:38:36 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,10 +14,7 @@ export ICON=https://github.com/yarl/pattypan/blob/master/src/pattypan/resources/
 # Deploy dependencies
 quick-sharun \
 	./AppDir/bin/* \
-	/usr/lib/jvm/java-26-openjdk/bin \
-    /usr/lib/jvm/java-26-openjdk/conf \
-    /usr/lib/jvm/java-26-openjdk/legal \
-    /usr/lib/jvm/java-26-openjdk/lib
+	/usr/lib/jvm/java-11-openjdk
 
 # Additional changes can be done in between here
 
