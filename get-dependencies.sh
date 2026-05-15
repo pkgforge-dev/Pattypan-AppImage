680ae89eb827d9b34f3549173951400f44d9d6f8
Link4Electronics <link4electronics@gmail.com>
2026-05-15 00:29:23 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,15 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm gradle jdk17-openjdk
+pacman -Syu --noconfirm \
+	gradle \
+	jdk17-openjdk
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package gradle8
 make-aur-package java-openjfx
 
 # If the application needs to be manually built that has to be done down here
